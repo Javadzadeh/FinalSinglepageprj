b9914d5afdf1c3563690d93f996d440037794b29
Soheila <Soheila@DESKTOP-E11FFP6>
2025-02-15 15:11:00 +0330

    ui changes

--- SinglePage.Sample01/obj/Debug/net8.0/SinglePage.Sample01.AssemblyInfo.cs
+++ SinglePage.Sample01/obj/Debug/net8.0/SinglePage.Sample01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SinglePage.Sample01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa874820de047f3111c5e1d43918fd308d93dcbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SinglePage.Sample01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SinglePage.Sample01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
